de434ccc6f138a87d3de98ed0eaa92c2b914e7c2
Muhammad Nasir <contactmnasirsaeed@gmail.com>
2023-09-02 04:16:19 +0500

    Code of Random Password Generator Has been written

--- Task 9_ Random Password Generator.py
+++ Task 9_ Random Password Generator.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+
 """
 Created on Sun Aug 20 00:17:47 2023
 
@@ -8,3 +8,14 @@ Created on Sun Aug 20 00:17:47 2023
 '''Task 9 : Random Password Generator
 Create a program that generates a random
 password of a user-defined length'''
+
+import random
+pass_length = int(input("Enter length of your password: "))
+password = ""
+
+for i in range (pass_length):
+   
+    string_of_password_characters = """abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
+    l = random.choice(string_of_password_characters)
+    password = password + l
+print("Your Random Stron Password is: ",password)    
